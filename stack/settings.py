34ddc465762cbd161dbc18726de92c02db6ccad5
habaoanh <habaoanh@gmail.com>
2018-04-13 01:04:17 -0700

    sua USER_AGENT

--- stack/settings.py
+++ stack/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'stack.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'stack (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
